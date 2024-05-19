b0780a09a589481eca13abf6a5eed004fda7f421
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-05-19 14:28:50 +0300

    intro 1 lesson

---
+++ main.js
@@ -0,0 +1,20 @@
+// data
+const info = {
+    title: 'it-incubator',
+    graduatesCount: 2000,
+    areYouChampion: true
+}
+
+// render for data
+const pageTitleElement = document.createElement('h1');
+pageTitleElement.append(info.title)
+document.body.append(pageTitleElement);
+
+const graduatesCountElement = document.createElement('input');
+graduatesCountElement.value =info.graduatesCount
+document.body.append(graduatesCountElement);
+
+const areYouChampionElement = document.createElement('input');
+areYouChampionElement.type = 'checkbox';
+areYouChampionElement.checked = info.areYouChampion;
+document.body.append(areYouChampionElement);
